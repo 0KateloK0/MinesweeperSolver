466f18cb6a0bc41c80785f5da61787e22a46f239
0KateloK0 <katelkin2003@mail.ru>
2022-08-14 23:50:41 +0300

    Failed attempt at React

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
-function App (props) {
-	return (
-		<div className="field">
-				
-		</div>
-		)
+const CELL_CLOSED = 0;
+const CELL_OPENED = 1;
+
+
+class Cell extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			status: CELL_CLOSED
+		};
+	}
+	render () {
+		return (
+			<div className={"field__cell field__cell_" + (this.state.status == CELL_CLOSED ? "closed" : "opened")}
+				onClick = {this.handleCellClick}
+			>
+			</div>
+			);
+	}
+	handleCellClick (e) {
+		this.setState({ status: CELL_OPENED });
+	}
+}
+
+class App extends React.Component {
+	constructor (props) {
+		super(props);
+	}
+	render () {
+		return (
+			<div className="field">
+				<Cell></Cell>
+			</div>
+		);
+	}
 }
 
 function renderReactApp (el) {
