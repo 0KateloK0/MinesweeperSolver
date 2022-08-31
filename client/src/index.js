4e8797c3e7d428a4a62a07b8d5684e2ac7cfe5fa
0KateloK0 <katelkin2003@mail.ru>
2022-09-01 02:00:20 +0300

    war crimes

--- client/src/index.js
+++ client/src/index.js
@@ -15,28 +15,78 @@ class App extends React.Component {
 			height: 10,
 			bombsAmount: 20,
 			solver: App.SOLVER_PLAYER,
-			gameStarted: true,
+			gameStarted: false,
 			incorrect_input: false
 		};
+	}
+	checkInputs () {
+		return true;
 	}
 	render () {
 		if (this.state.gameStarted) {
 			if (this.state.solver == App.SOLVER_PLAYER) {
 				return (
 					<div className="wrapper">
-						<Game width="10" height="10" bombsAmount="10"></Game>
+						<Game width={this.state.width} height={this.state.height} bombsAmount={this.state.bombsAmount} />
 					</div>
 				);
 			}
 			else {
-				return <div className="wrapper"></div>;
+				return <div className="wrapper">
+
+				</div>;
 				// make AI-interface
 			}
 		}
 		else {
 			return (
 				<div className="wrapper">
-					{/*make chosing of settings and the field for incorrect values*/}
+					<input type="number" onChange={
+						(e => {
+							this.setState({
+								width: e.target.value,
+								incorrect_input: false,
+							});
+						}).bind(this)
+					} value={this.state.width} />
+					<input type="number" onChange={
+						(e => {
+							this.setState({
+								height: e.target.value,
+								incorrect_input: false,
+							});
+						}).bind(this)
+					} value={this.state.height} />
+					<input type="number" onChange={
+						(e => {
+							this.setState({
+								bombsAmount: e.target.value,
+								incorrect_input: false,
+							});
+						}).bind(this)
+					} value={this.state.bombsAmount} />
+					<input type="checkbox" onChange={
+						(e => {
+							this.setState({
+								solver: e.target.value ? App.SOLVER_AI : App.SOLVER_PLAYER,
+								incorrect_input: false,
+							});
+						}).bind(this)
+					} value={this.state.solver == App.SOLVER_PLAYER} />
+					<button onClick={
+						(e => {
+							if (!this.checkInputs()) {
+								this.setState({
+									incorrect_input: true
+								});
+							}
+							else {
+								this.setState({
+									gameStarted: true
+								})
+							}
+						}).bind(this)
+					}>Start new game</button>
 				</div>
 				);
 		}
